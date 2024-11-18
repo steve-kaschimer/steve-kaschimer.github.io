deeefdbafb4fb64c1d972c46e84a34fa035ac772
Steve Kaschimer <steve.kaschimer@slalom.com>
2024-11-18 14:03:39 -0500

    Update banner.js

--- components/banner.js
+++ components/banner.js
@@ -17,6 +17,9 @@ export default function Banner({ pageName }) {
     case "blog":
       pageTitle = "Tech Notes";
       break;
+    default:
+      pageTitle = "Home";
+      break;
   }
   return (
     <div
